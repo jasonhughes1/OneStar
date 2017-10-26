2716bfbf9c58b7407c4f4e6401d283dead2b7c39
Jason Hughes <jasonhughes088@gmail.com>
2017-10-25 18:31:30 -0600

    Firebase has been hooked up

---
+++ one-star/src/firebase.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+import firebase from 'firebase'
+var config = {
+  apiKey: "AIzaSyB1lAyxK-IZVOvmxd_iLzzePILsxUwjhR0",
+  authDomain: "onestar-890b9.firebaseapp.com",
+  databaseURL: "https://onestar-890b9.firebaseio.com",
+  projectId: "onestar-890b9",
+  storageBucket: "onestar-890b9.appspot.com",
+  messagingSenderId: "539824320303"
+};
+firebase.initializeApp(config);
+export default firebase;
